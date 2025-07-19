93ce910be6ed275f3279ed156bb040e56e85b2e2
raj <rajneelamgaurav@gmail.com>
2025-07-19 20:42:40 +0530

    added backend port

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 import joblib
 from pydantic import BaseModel
 import pandas as pd
+import uvicorn
+import os
 
 model = joblib.load("backend/titanic_model.pkl")
 
@@ -28,3 +30,7 @@ def predict(data: Input):
     val = pd.DataFrame([[data.sex, int(data.age), int(data.pclass)]], columns=["Sex", "Age", "Pclass"])
     prediction = out = model.predict(val)
     return {"prediction": str("Survived" if prediction[0] else "Died")}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=port)
